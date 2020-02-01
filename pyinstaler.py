b6c7da40a7a407a2e549b0d8ff211a19b3342625
Сергей <polunec@ukr.net>
2020-02-01 17:27:43 +0200

    Bug fix

--- pyinstaler.py
+++ pyinstaler.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
-names = "Baza"
+names = "Профоблік"
 scripts = "Baza"
 icons = "Profico"
 num = 2
-path = '"C:/Program Files (x86)/Windows Kits/10/Redist/10.0.17763.0/ucrt/DLLs/x86"'
+path = '"C:/Program Files/Microsoft Visual Studio/Installer"'
 pathQt = 'C:/Python37/Lib/site-packages/PyQt5/Qt/bin'
 if str(num) == "1":
     pacs = "--onefile --console"
@@ -14,6 +14,6 @@ elif str(num) == "3":
     pacs = "--onefile --windowed"
 else:
     pacs = "--console"
-os.system("pyinstaller.exe  {0}  --icon={1}.ico  {2}.pyw --name={3} --paths={4} --paths={5} --debug=all".format(pacs, icons, scripts, names, path, pathQt))
+os.system("pyinstaller.exe  {0}  --icon={1}.ico  {2}.pyw --name={3} --paths={4} --paths={5}".format(pacs, icons, scripts, names, path, pathQt))
 
 # --debug
