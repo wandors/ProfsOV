1afebe93897b25a3b31ecf44e91f471b08da1a93
wandors <srpolunec@gmail.com>
2021-07-30 21:36:51 +0300

    Виправлення помилок

---
+++ Start.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Сергей Полунец'
+
+import sys
+import os
+from Baza import*
+
+
+class Main:
+    def __init__(self):
+        Run_prog()
+
+
+if __name__ == '__main__':
+    Main()
